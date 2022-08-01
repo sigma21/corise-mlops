94a714e2833f53e71b7a4a82035aaf46577e5acf
Smh <semihkocakel@hotmail.com>
2022-08-01 09:20:19 +0000

    constructs predict endpoint, updates model, refactors server.py

---
+++ app/model.py
@@ -0,0 +1,68 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+import joblib
+
+
+class TransformerFeaturizer(BaseEstimator, TransformerMixin):
+    def __init__(self, dim, sentence_transformer_model):
+        self.dim = dim
+        self.sentence_transformer_model = sentence_transformer_model
+
+    #estimator. Since we don't have to learn anything in the featurizer, this is a no-op
+    def fit(self, X, y=None):
+        return self
+
+    #transformation: return the encoding of the document as returned by the transformer model
+    def transform(self, X, y=None):
+        return self.sentence_transformer_model.encode([X])
+
+
+class NewsCategoryClassifier:
+    def __init__(self, config: dict) -> None:
+        self.config = config
+        """
+        [TO BE IMPLEMENTED]
+        1. Load the sentence transformer model and initialize the `featurizer` of type `TransformerFeaturizer` (Hint: revisit Week 1 Step 4)
+        2. Load the serialized model as defined in GLOBAL_CONFIG['model'] into memory and initialize `model`
+        """
+        sentence_transformer_model = SentenceTransformer('sentence-transformers/{model}'.format(model=config['model']['featurizer']['sentence_transformer_model']))
+        dim = config['model']['featurizer']['sentence_transformer_embedding_dim']
+
+        featurizer = TransformerFeaturizer(dim, sentence_transformer_model)
+        model = joblib.load(config['model']['classifier']['serialized_model_path'])
+       
+        self.pipeline = Pipeline([
+            ('transformer_featurizer', featurizer),
+            ('classifier', model)
+        ])
+
+    def predict_proba(self, model_input: dict) -> dict:
+        """
+        [TO BE IMPLEMENTED]
+        Using the `self.pipeline` constructed during initialization, 
+        run model inference on a given model input, and return the 
+        model prediction probability scores across all labels
+
+        Output format: 
+        {
+            "label_1": model_score_label_1,
+            "label_2": model_score_label_2 
+            ...
+        }
+        """
+        pred = self.pipeline.predict_proba(model_input)
+        return pred
+
+    def predict_label(self, model_input: dict) -> str:
+        """
+        [TO BE IMPLEMENTED]
+        Using the `self.pipeline` constructed during initialization,
+        run model inference on a given model input, and return the
+        model prediction label
+
+        Output format: predicted label for the model input
+        """
+        pred = self.pipeline.predict(model_input)
+        return pred
+
--- app/server.py
+++ app/server.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from loguru import logger
-import joblib
+from model import NewsCategoryClassifier
 
-from sentence_transformers import SentenceTransformer
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
 
 GLOBAL_CONFIG = {
     "model": {
@@ -14,11 +11,11 @@ GLOBAL_CONFIG = {
             "sentence_transformer_embedding_dim": 768
         },
         "classifier": {
-            "serialized_model_path": "./data/news_classifier.joblib"
+            "serialized_model_path": "../data/news_classifier.joblib"
         }
     },
     "service": {
-        "log_destination": "./data/logs.out"
+        "log_destination": "../data/logs.out"
     }
 }
 
@@ -34,78 +31,22 @@ class PredictResponse(BaseModel):
     label: str
 
 
-class TransformerFeaturizer(BaseEstimator, TransformerMixin):
-    def __init__(self, dim, sentence_transformer_model):
-        self.dim = dim
-        self.sentence_transformer_model = sentence_transformer_model
-
-    #estimator. Since we don't have to learn anything in the featurizer, this is a no-op
-    def fit(self, X, y=None):
-        return self
-
-    #transformation: return the encoding of the document as returned by the transformer model
-    def transform(self, X, y=None):
-        X_t = []
-        for doc in X:
-            X_t.append(self.sentence_transformer_model.encode(doc))
-        return X_t
-
-
-class NewsCategoryClassifier:
-    def __init__(self, config: dict) -> None:
-        self.config = config
-        """
-        [TO BE IMPLEMENTED]
-        1. Load the sentence transformer model and initialize the `featurizer` of type `TransformerFeaturizer` (Hint: revisit Week 1 Step 4)
-        2. Load the serialized model as defined in GLOBAL_CONFIG['model'] into memory and initialize `model`
-        """
-        featurizer = None
-        model = None
-        self.pipeline = Pipeline([
-            ('transformer_featurizer', featurizer),
-            ('classifier', model)
-        ])
-
-    def predict_proba(self, model_input: dict) -> dict:
-        """
-        [TO BE IMPLEMENTED]
-        Using the `self.pipeline` constructed during initialization, 
-        run model inference on a given model input, and return the 
-        model prediction probability scores across all labels
-
-        Output format: 
-        {
-            "label_1": model_score_label_1,
-            "label_2": model_score_label_2 
-            ...
-        }
-        """
-        return {}
-
-    def predict_label(self, model_input: dict) -> str:
-        """
-        [TO BE IMPLEMENTED]
-        Using the `self.pipeline` constructed during initialization,
-        run model inference on a given model input, and return the
-        model prediction label
-
-        Output format: predicted label for the model input
-        """
-        return ""
-
-
 app = FastAPI()
+classifier = NewsCategoryClassifier(GLOBAL_CONFIG)
+
 
 @app.on_event("startup")
 def startup_event():
     """
         [TO BE IMPLEMENTED]
-        2. Initialize the `NewsCategoryClassifier` instance to make predictions online. You should pass any relevant config parameters from `GLOBAL_CONFIG` that are needed by NewsCategoryClassifier 
-        3. Open an output file to write logs, at the destimation specififed by GLOBAL_CONFIG['service']['log_destination']
+        1. Initialize the `NewsCategoryClassifier` instance to make predictions online. You should pass any relevant config parameters from `GLOBAL_CONFIG` that are needed by NewsCategoryClassifier 
+        2. Open an output file to write logs, at the destination specified by GLOBAL_CONFIG['service']['log_destination']
         
         Access to the model instance and log file will be needed in /predict endpoint, make sure you
         store them as global variables
     """
+    classifier = NewsCategoryClassifier(GLOBAL_CONFIG)
+    # TODO! log file
     logger.info("Setup completed")
 
 
@@ -137,7 +78,18 @@ def predict(request: PredictRequest):
         }
         3. Construct an instance of `PredictResponse` and return
     """
-    return {}
+    pred = classifier.predict_label(request.description)[0]
+    pred_proba = classifier.predict_proba(request.description)[0]
+
+    class_names = classifier.pipeline.classes_
+    scores = dict(zip(class_names, pred_proba))
+
+    response_model = {
+        "scores": dict(sorted(scores.items(), key=lambda x: x[1], reverse=True)),
+        "label": pred
+    }
+
+    return response_model
 
 
 @app.get("/")
